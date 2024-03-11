b1c6789ad7aa6e433927c4fb9491d8bb6d63844a
s28581 <s28581@pjwstk.edu.pl>
2024-03-11 13:05:43 +0100

    test

---
+++ Solution1/cw2/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+        int[] tbl = { 1, 2, 3, 4, 5 };
+
+        foreach (int num in tbl)
+        {
+            
+        }
+    }
+}
