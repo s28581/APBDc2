37ceaa20838f7ea8a32a8cd3d156099fc2987ecb
s28581 <s28581@pjwstk.edu.pl>
2024-03-11 13:08:56 +0100

    mod 1

--- Solution1/cw2/Program.cs
+++ Solution1/cw2/Program.cs
@@ -4,10 +4,11 @@
     {
         Console.WriteLine("Hello");
         int[] tbl = { 1, 2, 3, 4, 5 };
-
+        
+        int sum = 0;
         foreach (int num in tbl)
         {
-            
+            sum += num;
         }
     }
 }
